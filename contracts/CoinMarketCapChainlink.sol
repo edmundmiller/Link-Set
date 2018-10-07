bd7a229bf833821f9263a98ff937f49e883bca3b
Edmund Miller <edmund.a.miller@gmail.com>
2018-10-07 05:11:44 -0500

    Finish Smart contracts

--- contracts/CoinMarketCapChainlink.sol
+++ contracts/CoinMarketCapChainlink.sol
@@ -9,8 +9,9 @@ contract CoinMarketCapChainlink is Chainlinked {
         // Set to the Ropsten addresses
         setLinkToken(0x20fE562d797A42Dcb3399062AE9546cd06f63280);
         setOracle(0xf0cef41e87a1218a775b5997b2bd76d8ad4104f1);
-        bytes32 constant SPEC_ID = bytes32("96b6a7fca7594887a3c3c3c0f9379e4e");
     }
+
+    bytes32 SPEC_ID = bytes32("96b6a7fca7594887a3c3c3c0f9379e4e");
 
     // Get the Coin Market Cap info for the coin
     function requestCoinMarketCapPrice(string _coin, string _market)
@@ -18,7 +19,7 @@ contract CoinMarketCapChainlink is Chainlinked {
         onlyOwner
         returns (bytes32 requestId)
     {
-        ChainlinkLib.Run memory run = newRun(SPEC_ID, this, "fulfill(bytes32,uint256)");
+        ChainlinkLib.Run memory run = newRun(SPEC_ID, this, "checkRequest(bytes32,uint256)");
         run.add("sym", _coin);
         run.add("convert", _market);
         string[] memory path = new string[](5);
