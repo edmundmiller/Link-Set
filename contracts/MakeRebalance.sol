652490895560fecb05144475ade97e052e36123a
Edmund Miller <edmund.a.miller@gmail.com>
2018-10-07 01:53:32 -0500

    Add MakeRebalance contract

---
+++ contracts/MakeRebalance.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.22;
+
+import "./CoinMarketCapChainlink.sol"
+
+contract MakeRebalance is CoinMarketCapChainlink {
+    string daiAPICode = "2308"
+    string chainlinkAPICode = "1975"
+
+    // address
+    function rebalance {
+    }
+}
